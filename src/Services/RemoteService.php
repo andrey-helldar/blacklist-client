0bacc92d3ef71866456d162a7b9a24bebfa37b9f
Andrey Helldar <helldar@ai-rus.com>
2019-09-08 23:58:06 +0300

    An attempt to transfer `HttpClient` from Client to the Core.

--- src/Services/RemoteService.php
+++ src/Services/RemoteService.php
@@ -2,26 +2,12 @@
 
 namespace Helldar\BlacklistClient\Services;
 
-use GuzzleHttp\Client;
 use Helldar\BlacklistClient\Contracts\Service;
 use Helldar\BlacklistCore\Constants\Server;
+use Helldar\BlacklistCore\Facades\HttpClient;
 
 class RemoteService implements Service
 {
-    private $client;
-
-    public function __construct(Client $client)
-    {
-        $this->client = $client;
-    }
-
-    /**
-     * @param string|null $source
-     * @param string|null $type
-     *
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     * @return string
-     */
     public function store(string $source = null, string $type = null)
     {
         $response = $this->send('POST', \compact('type', 'source'));
@@ -29,13 +15,6 @@ class RemoteService implements Service
         return $response->getBody()->getContents();
     }
 
-    /**
-     * @param string|null $source
-     * @param string|null $type
-     *
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     * @return bool
-     */
     public function check(string $source = null, string $type = null): bool
     {
         $response = $this->send('GET', \compact('type', 'source'));
@@ -43,21 +22,17 @@ class RemoteService implements Service
         return $response->getBody()->getContents();
     }
 
-    /**
-     * @param string $method
-     * @param array $form_params
-     *
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     * @return mixed|\Psr\Http\Message\ResponseInterface
-     */
-    private function send(string $method, array $form_params)
+    private function send(string $method, array $data)
     {
         $base_uri = \config('blacklist_client.server_url', Server::BASE_URL);
-        $verify   = \config('blacklist_client.verify_ssl', true);
         $timeout  = \config('blacklist_client.server_timeout', 0);
-        $headers  = Server::HEADERS;
+        $verify   = \config('blacklist_client.verify_ssl', true);
+        $headers  = \config('blacklist_client.headers', true);
 
-        return $this->client
-            ->request($method, \compact('base_uri', 'verify', 'timeout', 'headers', 'form_params'));
+        return HttpClient::setBaseUri($base_uri)
+            ->setTimeout($timeout)
+            ->setVerify($verify)
+            ->setHeaders($headers)
+            ->send($method, $data);
     }
 }
