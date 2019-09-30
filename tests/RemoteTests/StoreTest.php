69915395b11969113922c5e36762abe2ff7f7968
Andrey Helldar <helldar@ai-rus.com>
2019-09-30 21:47:29 +0300

    Updated tests

--- tests/RemoteTests/StoreTest.php
+++ tests/RemoteTests/StoreTest.php
@@ -51,17 +51,17 @@ class StoreTest extends TestCase
 
     public function testSelfBlockingIp()
     {
-        $this->expectException(SelfBlockingDetected::class);
-        $this->expectExceptionMessage('You are trying to block yourself! (127.0.0.1)');
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(400);
 
         Client::store('127.0.0.1', 'ip');
     }
 
     public function testSelfBlockingHost()
     {
-        $this->expectException(SelfBlockingDetected::class);
-        $this->expectExceptionMessage('You are trying to block yourself! (localhost)');
+        $this->expectException(ClientException::class);
+        $this->expectExceptionCode(400);
 
-        Client::store('localhost', 'ip');
+        Client::store('http://localhost', 'host');
     }
 }
