10993d16c69790a757895ecf32294122891e0251
Andrey Helldar <helldar@ai-rus.com>
2019-10-03 14:42:26 +0300

    Added the `Arr::wrap()` method

--- vendor/andrey-helldar/blacklist-core/src/Helpers/Arr.php
+++ vendor/andrey-helldar/blacklist-core/src/Helpers/Arr.php
@@ -2,6 +2,8 @@
 
 namespace Helldar\BlacklistCore\Helpers;
 
+use function is_array;
+
 class Arr
 {
     public static function add($array, string $key, $value): array
@@ -12,12 +14,28 @@ class Arr
     /**
      * @param array $array
      * @param string $key
-     * @param null|mixed $default
+     * @param mixed|null $default
      *
      * @return mixed|null
      */
     public static function get(array $array, string $key, $default = null)
     {
         return $array[$key] ?? $default;
+    }
+
+    /**
+     * If the given value is not an array and not null, wrap it in one.
+     *
+     * @param mixed $value
+     *
+     * @return array
+     */
+    public static function wrap($value)
+    {
+        if (null === $value) {
+            return [];
+        }
+
+        return is_array($value) ? $value : [$value];
     }
 }
